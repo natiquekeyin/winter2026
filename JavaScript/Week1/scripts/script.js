180aea68f3d514cd9acff382dabdc2d04e3c1163
Noman Atique <noman.atique@keyin.com>
2026-01-08 11:26:29 -0500

    jan8

---
+++ JavaScript/Week1/scripts/script.js
@@ -0,0 +1,21 @@
+// console.log("Hello Keyin College");
+// console.log("This is a new line");
+
+// let customerCount = 10;
+// let cUstomerCount = 20;
+
+// let x = 10;
+// let X = 20;
+
+let customerCount = 10; // camelCase JS Prefers this!
+let CustomerCount = 10; // PascalCase
+let customer_count = 10; // snake_case
+// let customer-count=10;// kebab-case
+
+let x = 10;
+let y = 20;
+
+//this is single line comment
+/* this is a multiline
+comment going to diffent 
+lines */
