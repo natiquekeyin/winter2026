1acd7993404fce1dc6a03d0a712959b15f86bbfc
Noman Atique <noman.atique@keyin.com>
2026-01-12 11:30:05 -0500

    jan12_JS

--- JavaScript/Week1/scripts/script.js
+++ JavaScript/Week1/scripts/script.js
@@ -1,21 +1,166 @@
-// console.log("Hello Keyin College");
-// console.log("This is a new line");
+// // /*
+// // Name: Keyin College
+// // Studnet reg ID: 12345
+// // Assignment: Practice Set 1 / QAP1 /Sprint 1/ Final Sprint
+// // Date: January 12,2026
+// // */
 
-// let customerCount = 10;
-// let cUstomerCount = 20;
+// // // console.log("Hello Keyin College");
+// // // console.log("This is a new line");
 
-// let x = 10;
-// let X = 20;
+// // // let customerCount = 10;
+// // // let cUstomerCount = 20;
 
-let customerCount = 10; // camelCase JS Prefers this!
-let CustomerCount = 10; // PascalCase
-let customer_count = 10; // snake_case
-// let customer-count=10;// kebab-case
+// // // let x = 10;
+// // // let X = 20;
 
-let x = 10;
-let y = 20;
+// // let customerCount = 10; // camelCase JS Prefers this!
+// // let CustomerCount = 10; // PascalCase
+// // let customer_count = 10; // snake_case
+// // // let customer-count=10;// kebab-case
 
-//this is single line comment
-/* this is a multiline
-comment going to diffent 
-lines */
+// // let x = 10;
+// // let y = 20;
+
+// // //this is single line comment
+// // /* this is a multiline
+// // comment going to diffent
+// // lines */
+
+// // console.log(x + y);
+// // console.log("Hello Keyin College");
+
+// // {
+// //   console.log("inside block");
+// //   console.log("another inside block");
+// // }
+
+// // if (x < 10) {
+// //   console.log("X is less than 10");
+// //   console.log("You are studying hard");
+// // } else {
+// //   console.log("hello world i am else");
+// // }
+
+// // function doSomething() {
+// //   console.log("This is do something functions");
+// // }
+
+// // doSomething();
+
+// // let x = 10;
+// // let name = "Alice";
+
+// // int x = 20; (int means it is going to be an integer ONLY)
+// // char y = 'A'; (i can only store a character in y now)
+// // STRONGLY TYPED LANGUAGUES
+
+// // let x = 10;
+// // x = "Alice";
+// // x = true;
+// // x = 2.3;
+// // Loosely Typed Languages
+// // typeof ???
+
+// // let x;
+// // console.log(typeof x);
+// // x = 10;
+// // console.log(typeof x);
+// // x = "Alice";
+// // console.log(typeof x);
+// // x = true;
+// // console.log(typeof x);
+
+// // var x = 10;
+// // const y = 20;
+
+// // let _x = 10;
+// // let x = 10;
+// // let $x = 10;
+// // let 4x = 3; WRONG
+
+// // let let=10;
+// // because let is reserved or keyword of JS langauge!
+
+// // let const = 3;
+
+// // let function = 3; funciton is  a reserved or keyword or JS
+
+// let x = function () {
+//   console.log("hello");
+// };
+
+// console.log(typeof x); //function/object data type
+// //
+
+// let today = new Date();
+// console.log(today);
+// console.log(typeof today);
+
+// let x;
+// console.log(typeof x);
+
+// x = null;
+// console.log(typeof x);
+
+// let x = "hello";
+// console.log(typeof x);
+
+// x = "123";
+// console.log(typeof x);
+
+// x = "123";
+// console.log(typeof x);
+
+// let x = 123 + 4 + 5 + 6;
+// console.log(x);
+
+// let d = null;
+
+// console.log(d);
+// console.log(typeof d);
+
+// let x = 11;
+// console.log(x % 2);
+
+// // console.log(10%2)
+// x = 12;
+// if (x % 2 == 0) {
+//   console.log("This is even number");
+// }
+
+// let x = 1;
+// // x = x + 5;
+// x += 5;
+// console.log(x);
+
+// ++ is called "increment operator" -- is called "decrement operator"
+
+// let x = 1;
+// x++;
+// console.log(x);
+
+// let y = 1;
+// y--;
+// console.log(y);
+
+// Increment/decrement operators are itself of two types:
+// Pre-Incremen/decrement & Post-increment/decrement
+
+// let x = 1;
+// // console.log(x++); //post increment
+// console.log(++x);
+// console.log(x);
+
+// let x = 1;
+// console.log(x++); //1  int: 2
+// console.log(++x); //3  int: 3
+// console.log(x++); //3  int:4
+// console.log(x); //4     int:4
+
+let z = 10;
+console.log(z--); //10
+console.log(--z); //8
+console.log(z--); // 8
+console.log(z); //7
+console.log(--z); //6
