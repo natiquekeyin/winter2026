1c2cb98fe6ebd693bd4e35ad1af610b79695d55c
Noman Atique <noman.atique@keyin.com>
2026-01-29 11:33:18 -0500

    Jan29_1

--- JavaScript/Week4/scripts/script.js
+++ JavaScript/Week4/scripts/script.js
@@ -28,7 +28,51 @@
 // // Destructuring method
 // let [lat, lng] = position;
 
-let dateString = `2026/01/27`;
+// let dateString = `2026/01/27`;
 
-let [year, , day] = dateString.split("/");
-console.log(year, day);
+// let [year, month, day] = dateString.split("/");
+// console.log(year, month, day);
+
+// let nums = [1, 2, 3, 4, 5];
+// Read each value from nums and mulitply it with 3 and store it in newNums
+// let newNums = [];
+// By using for loop
+// for (let i = 0; i < nums.length; i++) {
+//   let value = nums[i];
+//   value = value * 3;
+//   newNums.push(value);
+// }
+
+// by using forEach()
+
+// nums.forEach(function (value) {
+//   value *= 3;
+//   newNums.push(value);
+// });
+
+// let newNums = nums.map(function (value) {
+//   return value * 3;
+// });
+// let newNums = nums.map((value) => value * 3);
+/*
+map() creates a new array from calling a function for every array element.
+map() does not execute the function for empty elements.
+map() does not change the original array.
+*/
+// console.log(nums, newNums);
+
+// filter function!
+
+// let list = [15, 12, 33, 14];
+// let newList = list.filter((value) => value <= 15);
+// let newList2 = list.map((value) => value <= 15);
+// console.log(list, newList, newList2);
+
+let fruits = ["apple", "banana", "orange", "abc", "abcdef"];
+let newFruits = fruits.map((fruit) => fruit.startsWith("b"));
+let newFruits2 = fruits.filter((fruit) => fruit.startsWith("b"));
+let newFruits3 = fruits.every((fruit) => fruit.startsWith("b"));
+console.log(fruits);
+console.log(newFruits);
+console.log(newFruits2);
+console.log(newFruits3);
