844cd709a060eb0236193953687d338a076545c7
Noman Atique <noman.atique@keyin.com>
2026-02-18 11:12:07 -0500

    feb18

--- JavaScript/Week7/scripts/fetch.js
+++ JavaScript/Week7/scripts/fetch.js
@@ -24,10 +24,61 @@ btn1.addEventListener("click", function () {
 });
 
 let btn2 = document.querySelector("#btn2");
-btn2.addEventListener("click", function () {});
+btn2.addEventListener("click", function () {
+  fetch("./data/user.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      document.querySelector("#output").innerHTML =
+        `<div class="profile"><p>${data.id}</p><p>${data.name}</p><p>${data.age}</p><p>${data.email}</p></div>`;
+    });
+});
 
 let btn3 = document.querySelector("#btn3");
-btn3.addEventListener("click", function () {});
+btn3.addEventListener("click", function () {
+  fetch("./data/users.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((user) => {
+        document.querySelector("#output").innerHTML +=
+          `<div class="profile"><h1>${user.id}</h1><p>${user.name}</p><p>${user.age}</p><p>${user.email}</p></div>`;
+      });
+    });
+});
 
 let btn4 = document.querySelector("#btn4");
-btn4.addEventListener("click", function () {});
+btn4.addEventListener("click", function () {
+  fetch("https://api.github.com/users")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((user) => {
+        document.querySelector("#output").innerHTML += `<div class="profile">
+          <h1>
+          ${user.login}</h1>
+          <img src = "${user.avatar_url}" class="profilePic" alt="Profile"/>
+          <a href = "${user.html_url}">${user.html_url}</a>         
+          </div>`;
+      });
+    });
+});
+
+let btn5 = document.querySelector("#btn5");
+btn5.addEventListener("click", function () {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((responses) => {
+      return responses.json();
+    })
+    .then((data) => {
+      data.forEach((user) => {
+        document.querySelector("#output").innerHTML +=
+          `<div class="profile"><h1>ID:${user.id}</h1><p>Name:${user.name}</p><p>Email:${user.email}</p>
+          <p>Address: Street${user.address.street}</p><p>Geo:${user.address.geo.lat}</p><p><a href="http://www.${user.website}">${user.website}</a>
+          </div>`;
+      });
+    });
+});
