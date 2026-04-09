34accac4ccd27c2113b5b5840fa212bb67cff2eb
Noman Atique <noman.atique@keyin.com>
2026-04-09 12:08:01 -0400

    lastCommit

--- JavaScript/Week13/testing/src/components/counter/Counter.test.jsx
+++ JavaScript/Week13/testing/src/components/counter/Counter.test.jsx
@@ -22,6 +22,31 @@ describe("Counter", () => {
     await user.click(incrementButton);
     await user.click(incrementButton);
     const countElement = screen.getByRole("heading");
-    expect(countElement).toHaveTextContent("1");
+    expect(countElement).toHaveTextContent("2");
+  });
+
+  test("renders a count of 10 after user enters 10 and clicks set button", async () => {
+    const amountInput = screen.getByRole("spinbutton");
+    await user.type(amountInput, "200");
+    expect(amountInput).toHaveValue(200);
+    const setButton = screen.getByRole("button", { name: "Set" });
+    await user.click(setButton);
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("20");
+  });
+
+  test("elements are focused in the right order", async () => {
+    const amountInput = screen.getByRole("spinbutton");
+    const setButton = screen.getByRole("button", { name: "Set" });
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
+    await user.tab();
+    await user.tab();
+    expect(incrementButton).toHaveFocus();
+    await user.tab();
+    expect(amountInput).toHaveFocus();
+    await user.tab();
+    expect(setButton).toHaveFocus();
   });
 });
+
+// Coercion???  10 == "10" TRUE | 10 === "10" FALSE
